5d153469d5359a316bc5d65b26610a484aaf676e
osbe0012 <chris.osberg@gmail.com>
2020-11-16 15:25:43 -0500

    Driver catches invalid input exceptions

--- OsbergJDBCBank/src/main/java/com/revature/driver/Driver.java
+++ OsbergJDBCBank/src/main/java/com/revature/driver/Driver.java
@@ -10,33 +10,36 @@ import com.revature.exception.InvalidInputException;
 public class Driver {
 	private static Role role = new Role();
 	public static Scanner scanner = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to JavaBank Online");
-		while(queryService()) {}
+		while (queryService()) {
+		}
 		System.out.println("Thank you for using JavaBank Online. Have a nice day!");
 	}
 
 	public static boolean queryService() {
 		Service[] servicesArray = MenuFormatter.displayMainMenu(role);
 		String line = scanner.nextLine();
-		return useInput(line, servicesArray);
+		try {
+			return useInput(line, servicesArray);
+		} catch (InvalidInputException e) {
+			return true;
+		}
 	}
-	
+
 	private static boolean useInput(String line, Service[] servicesArray) {
 		boolean continueServices = true;
 		int choice;
-		try{
-			choice = Integer.parseInt(line)-1;
-		}
-		catch(Exception e) {
+		try {
+			choice = Integer.parseInt(line) - 1;
+		} catch (Exception e) {
 			throw new InvalidInputException("Exception: Invalid input");
 		}
-		if(-1<choice && choice<servicesArray.length) {
+		if (-1 < choice && choice < servicesArray.length) {
 			continueServices = servicesArray[choice].performService(role);
 			return continueServices;
-		}
-		else {
+		} else {
 			throw new InvalidInputException("Exception: Invalid input");
 		}
 	}
